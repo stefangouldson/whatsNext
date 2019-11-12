8db1705a8688b152d7d4ddec318f3c0061fba628
Stefan Gouldson <stefangouldson@gmail.com>
2019-11-12 11:19:47 +0000

    not using React anymore

--- src/App.js
+++ src/App.js
@@ -6,29 +6,64 @@ class App extends React.Component{
   state = {
     page:"Home"
   }
-  render(){
-    return(
-      <div id="page">
-        <nav>
-          <div id="logo-box">
-          <h1 id="logo">What's Next</h1>
-          </div>
-          <div id="links-box">
-            <h1 className="navLinks">Home</h1>
-            <h1 className="navLinks">Members</h1>
-            <h1 className="navLinks">Dates</h1>
-            <h1 className="navLinks">Reviews</h1>
-          </div>
-        </nav>
 
-      <img className="background-img" src={pic}></img>
-
-      <footer>
-        <h1>Email: email@example.com</h1>
-      </footer>
-      </div>
+  render(){return(
+  <div className="Pages">
+    <Navbar/>
+    <Homepage/>
+    <Memberspage/>
+    <MemberRow photo="pic" text="This is one"/>
+    <Footer/>
+    </div>
     )
   }
 }
+
+const Navbar = () =>{
+  return (
+    <nav>
+      <h1>Home</h1>
+      <h1>Members</h1>
+      <h1>Songs</h1>
+      <h1>Dates</h1>
+    </nav>
+  )
+}
+
+const Homepage = () =>{
+  return(
+  <div className="page">
+  <img className="background-img" src={pic} alt=""></img>
+  </div>
+  )
+}
+
+const Memberspage = () =>{
+  return (
+    <div className="page">
+    <h2>Meet The Band</h2>
+    <p>What's Next is an amazing band playing in the North West with an excellent group of musicians able to play great music and entertain crowds of people</p>
+    </div>
+  )
+}
+
+const MemberRow = (props) =>{
+  return (
+    <div>
+      <img className="memberPhoto" src={props.photo} alt=""></img>
+      <p>{props.text}</p>
+    </div>
+  )
+}
+
+const Footer = () =>{
+  return (
+    <footer>
+    <h1>Email: email@example.com</h1>
+    </footer>
+  )
+}
+
+
 
 export default App;
