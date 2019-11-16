3c3051d42a879eb3677d07b720d49742924b3260
Stefan Gouldson <stefangouldson@gmail.com>
2019-11-16 19:27:04 +0000

    Updates

---
+++ precache-manifest.4aa3069c9871ab7d260744a18a303682.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0819906d50b8a643405343e3a85f1234",
+    "url": "/whatsNext/index.html"
+  },
+  {
+    "revision": "b3d14100377580f54fbd",
+    "url": "/whatsNext/static/css/main.fa1b76a7.chunk.css"
+  },
+  {
+    "revision": "72f0f8d589b86360d36b",
+    "url": "/whatsNext/static/js/2.fb5b33a6.chunk.js"
+  },
+  {
+    "revision": "b3d14100377580f54fbd",
+    "url": "/whatsNext/static/js/main.328af7b8.chunk.js"
+  },
+  {
+    "revision": "abd1a69ddf7b4aa1eab9",
+    "url": "/whatsNext/static/js/runtime-main.161d00b7.js"
+  },
+  {
+    "revision": "ffd471955f3cad9ee787e79372d7b3e7",
+    "url": "/whatsNext/static/media/bandPic.ffd47195.jpg"
+  }
+]);
